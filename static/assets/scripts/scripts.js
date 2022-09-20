cbfcd340c1fb7052dd64650e033d7e3d6af6d2a2
vivian <vivian.v18723@gmail.com>
2022-09-20 22:58:09 +0100

    add content & styles to add task page

--- static/assets/scripts/scripts.js
+++ static/assets/scripts/scripts.js
@@ -4,4 +4,13 @@ $(document).ready(function () {
   $(".collapsible").collapsible();
 
   $(".tooltipped").tooltip();
+
+  $(".datepicker").datepicker({
+    format: "dd mmmm, yyyy",
+    yearRange: 3,
+    showClearBtn: true,
+    i18n: {
+      done: "Select",
+    }
+  });
 });
